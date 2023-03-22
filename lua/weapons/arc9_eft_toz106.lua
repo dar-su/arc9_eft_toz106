802ff590b2e8d42b3939d02f04c62d67992148a6
Darsu <95997959+dar-su@users.noreply.github.com>
2023-03-22 20:41:45 +0700

    ergo breath and ads volume

--- lua/weapons/arc9_eft_toz106.lua
+++ lua/weapons/arc9_eft_toz106.lua
@@ -133,8 +133,13 @@ SWEP.Sway = 1
 SWEP.SwayMove = 0.5
 SWEP.SwayMidAir = 10
 SWEP.SwayMultCrouch = 0.75
-SWEP.SwayMultHipFire = 0.01
-SWEP.SwayMultSights = 0.15
+SWEP.SwayMultHipFire = 0.2
+SWEP.SwayMultSights = 0.25
+SWEP.HoldBreathTime = 40
+SWEP.RestoreBreathTime = 30
+SWEP.BreathInSound = false 
+SWEP.BreathOutSound = false
+SWEP.BreathRunOutSound = "arc9_eft_shared/bear3_breath_sprint.wav"
 
 
 --          Generic stats
@@ -259,6 +264,9 @@ SWEP.FiremodeSound = "" -- we will have own in sound tables
 SWEP.ToggleAttSound = "" -- we will have own in sound tables
 SWEP.DryFireSound = "" -- we will have own in sound tables
 
+
+SWEP.EnterSightsSound = "arc9_eft_shared/weap_in.wav"
+SWEP.ExitSightsSound = "arc9_eft_shared/weap_handoff.wav"
 
 ------------------------- [[[           Hooks & functions            ]]] -------------------------
 
@@ -864,6 +872,11 @@ SWEP.Attachments = {
 SWEP.EFTErgo = 60
 if ARC9EFTBASE then
     SWEP.AimDownSightsTimeHook = ARC9EFT.ErgoHook
+    if ARC9EFT.ErgoBreathHook then
+        SWEP.HoldBreathTimeHook = ARC9EFT.ErgoBreathHook
+        SWEP.HookP_TranslateSound = ARC9EFT.ErgoAdsVolume
+    end
 else
     print("Dum! install arc9 eft shared!!!!!!!!!!!!!!")
 end
+SWEP.AimDownSightsTimeMultShooting = 4
