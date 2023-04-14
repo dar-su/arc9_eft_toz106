efc6457fca6ccbf0ab48387c8c8e700c27f09969
Darsu <95997959+dar-su@users.noreply.github.com>
2023-04-14 19:52:03 +0700

    spread bonus

--- lua/weapons/arc9_eft_toz106.lua
+++ lua/weapons/arc9_eft_toz106.lua
@@ -50,7 +50,7 @@ SWEP.Num = 8
 SWEP.Spread = 0.01113
 SWEP.UseDispersion = true
 SWEP.DispersionSpread = 0.005
-SWEP.DispersionSpreadAddHipFire = 0.02
+SWEP.DispersionSpreadAddHipFire = 0.03
 SWEP.DispersionSpreadMultMove = 1.5
 SWEP.DispersionSpreadAddMove = 0.015
 
@@ -874,11 +874,10 @@ SWEP.Attachments = {
 
 SWEP.EFTErgo = 60
 if ARC9EFTBASE then
-    SWEP.AimDownSightsTimeHook = ARC9EFT.ErgoHook
-    if ARC9EFT.ErgoBreathHook then
-        SWEP.HoldBreathTimeHook = ARC9EFT.ErgoBreathHook
-        SWEP.HookP_TranslateSound = ARC9EFT.ErgoAdsVolume
-    end
+    SWEP.AimDownSightsTimeHook = ARC9EFT.ErgoHook or nil
+    SWEP.HoldBreathTimeHook = ARC9EFT.ErgoBreathHook or nil
+    SWEP.HookP_TranslateSound = ARC9EFT.ErgoAdsVolume or nil
+    SWEP.SpreadHook = ARC9EFT.SpreadBonus or nil
 else
     print("Dum! install arc9 eft shared!!!!!!!!!!!!!!")
 end
